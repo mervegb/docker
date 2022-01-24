9b5f161369ae57b9a4edf38b4275572fba6d836a
mervegb <merveguzel.baykara@gmail.com>
2022-01-24 18:43:25 +0300

    added depends on

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
-
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 mongoose
-  .connect("mongodb://merve:mypassword@mongo:27017/?authSource=admin")
+  .connect(mongoUrl)
   .then(() => console.log("Connected to Database"))
   .catch((error) => console.log(error));
 
