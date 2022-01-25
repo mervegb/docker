21c029e3bac047c59bd9a37e2033efaa3f9facd2
mervegb <merveguzel.baykara@gmail.com>
2022-01-25 13:43:47 +0300

    fixed bcrpyt compare functionality

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -21,7 +21,7 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ username: username });
     !user && res.status(404).json("User not found");
 
-    const isCorrect = await bcrypt.compare(password, user.password);
+    const isCorrect = bcrypt.compare(password, user.password);
     if (isCorrect) {
       res.status(200).json(user);
     } else {
